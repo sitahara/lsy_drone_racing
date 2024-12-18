8574c1a4107fa1422fa4d32e3f05324248a254e0
Luca Worbis <188868464+cryxil@users.noreply.github.com>
2024-12-18 13:13:07 +0100

    Acados Update

--- lsy_drone_racing/control/mpc_acados.py
+++ lsy_drone_racing/control/mpc_acados.py
@@ -2,6 +2,7 @@
 
 import casadi as ca
 import numpy as np
+import pybullet as p
 import scipy as sp
 from acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
 from numpy.typing import NDArray
@@ -29,8 +30,8 @@ class MPCController(BaseController):
         super().__init__(initial_obs, initial_info)
 
         # Inital parameters
-        self.useTorqueModel = False  # whether to use the torque model
-        self.n_step = 0  # current step for the target trajectory
+        # whether to use the torque (total thrust + body torques) or thrust model (individual rotor thrusts)
+        self.useTorqueModel = False
         self.t_step = 1 / 60  # Time step, 60Hz
         self.n_horizon = 60  # Prediction horizon, 1s
         self.nx = 12  # number of states
@@ -39,10 +40,11 @@ class MPCController(BaseController):
         self.soft_constraints = False  # whether to use soft constraints
         self.soft_penalty = 1e3  # penalty for soft constraints
         self.Qs = np.diag(
-            [1, 1, 1, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.1, 0.1, 0.1]
-        )  # stage cost
+            [1, 1, 1, 0, 0, 0, 0, 0, 0, 0.01, 0.01, 0.01]
+        )  # stage cost for states (position, velocity, euler angles, angular velocity)
         self.Qt = self.Qs  # terminal cost
-        self.Rs = np.diag([1, 1, 1, 1])  # control cost
+        self.Rs = np.diag([0.01, 0.01, 0.01, 0.01]) * 0  # control cost
+        self.Rsdelta = np.diag([0.1, 0.1, 0.1, 0.1]) * 0  # control rate cost
         # For warm start
         self.x_guess = None
         self.u_guess = None
@@ -85,7 +87,6 @@ class MPCController(BaseController):
         """Reset the MPC controller to its initial state."""
         self.x_guess = None
         self.u_guess = None
-        self.n_step = 0
 
         self.model = None
         self.ocp = None
@@ -134,9 +135,12 @@ class MPCController(BaseController):
 
         # Check whether initial guess is available
         if self.x_guess is None or self.u_guess is None:
-            # self.calculateInitialGuess(current_state)
-            self.x_guess = np.zeros((self.nx, self.n_horizon + 1))
-            self.u_guess = np.zeros((self.nu, self.n_horizon))
+            target_trajectory = self.updateTargetTrajectory()
+            self.x_guess = np.vstack([target_trajectory, np.zeros((9, self.n_horizon + 1))])
+            self.u_guess = np.ones((self.nu, self.n_horizon)) * self.u_eq[0]
+            print("Initial guess", self.x_guess[:, 0:3], self.u_guess[:, 0:3])
+            # self.u_guess = np.zeros((self.nu, self.n_horizon))
+
             for k in range(self.n_horizon):
                 self.ocp_solver.set(k, "x", self.x_guess[:, k])
                 self.ocp_solver.set(k, "u", self.u_guess[:, k])
@@ -146,12 +150,10 @@ class MPCController(BaseController):
         pos_des = self.updateTargetTrajectory()
 
         # Set the reference trajectory for stage and terminal states
+        # Correct update checked
         for k in range(self.n_horizon):
-            self.ocp_solver.set(
-                k,
-                "yref",
-                np.concatenate([pos_des[:, k], np.zeros(9), np.array(self.u_eq).flatten()]),
-            )
+            y_ref = np.concatenate([pos_des[:, k], np.zeros(9), self.u_eq])
+            self.ocp_solver.set(k, "yref", y_ref)
         self.ocp_solver.set(
             self.n_horizon, "yref", np.concatenate([pos_des[:, self.n_horizon], np.zeros(9)])
         )
@@ -162,10 +164,11 @@ class MPCController(BaseController):
             self.ocp_solver.print_statistics()
             raise Exception(f"acados failed with status {status}. Exiting.")
         if status == 2:
-            print(f"acados returned status {status}. ")
+            # print(f"acados returned status {status}. ")
+            pass
 
         # Extract the control input
-        u = self.ocp_solver.get(0, "u")  # noqa: F841
+        u = self.ocp_solver.get(0, "u")
 
         # Store the current solution as the initial guess for the next iteration
         self.x_guess = np.zeros((self.nx, self.n_horizon + 1))
@@ -174,7 +177,7 @@ class MPCController(BaseController):
             self.x_guess[:, k] = self.ocp_solver.get(k, "x")
             self.u_guess[:, k] = self.ocp_solver.get(k, "u")
         self.x_guess[:, self.n_horizon] = self.ocp_solver.get(self.n_horizon, "x")
-
+        print("Guessed position: ", self.x_guess[0:3, :10])
         # Extract the next predicted states from the solver
         next_pos = self.ocp_solver.get(1, "x")[0:3]
         next_vel = self.ocp_solver.get(1, "x")[3:6]
@@ -191,6 +194,10 @@ class MPCController(BaseController):
         action = np.concatenate(
             [next_pos, next_vel, acc, [next_eul_ang[2]], next_deul_ang.flatten()]
         )
+        action = np.concatenate([next_pos, np.zeros(10)])
+        print(f"Current position: {pos}")
+        print(f"Desired position: {pos_des[:, 0]}")
+        print(f"Next position: {next_pos}")
 
         # self.tick = (self.tick + 1) % self.cycles_to_update
         return action.flatten()
@@ -222,7 +229,7 @@ class MPCController(BaseController):
             thrust = ca.MX.sym("thrust", 1)  # total thrust
             torques = ca.MX.sym("torques", 3)  # body frame torques
             model.u = ca.vertcat(thrust, torques)  # control input
-            self.u_eq = ca.vertcat(self.mass * self.g, 0, 0, 0)  # equilibrium control input
+            self.u_eq = np.array([self.mass * self.g, 0, 0, 0]).T  # equilibrium control input
 
             # Define Dynamics in world frame as euler angles
             w = W1(eul_ang) @ deul_ang  # Body Angular velocity
@@ -253,7 +260,7 @@ class MPCController(BaseController):
             model.u = ca.vertcat(f1, f2, f3, f4)  # control
 
             eq = 0.25 * self.mass * self.g  # equilibrium control input per motor
-            self.u_eq = ca.vertcat(eq, eq, eq, eq)  # equilibrium control input, drone hovers
+            self.u_eq = np.array([eq, eq, eq, eq]).T  # equilibrium control input, drone hovers
             beta = self.arm_length / ca.sqrt(2.0)  # beta = l/sqrt(2)
             torques = ca.vertcat(
                 beta * (f1 + f2 - f3 - f4),
@@ -293,7 +300,7 @@ class MPCController(BaseController):
         # Set solver options
         ocp.solver_options.N_horizon = self.n_horizon
         ocp.solver_options.tf = self.n_horizon * self.t_step
-        ocp.solver_options.integrator_type = "DISCRETE"  # "ERK", "IRK", "GNSF"
+        ocp.solver_options.integrator_type = "ERK"  # "ERK", "IRK", "GNSF"
         ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"  # FULL_CONDENSING_QPOASES
         ocp.solver_options.hessian_approx = "GAUSS_NEWTON"  # "EXACT", "GAUSS_NEWTON"
         ocp.solver_options.nlp_solver_type = "SQP"  # SQP, SQP_RTI
@@ -313,7 +320,6 @@ class MPCController(BaseController):
         ocp.cost.yref_e = np.zeros(
             (self.nx,)
         )  # dummy reference, only states are considered for terminal cost
-
         ocp.cost.W = sp.linalg.block_diag(self.Qs, self.Rs)  # Weight matrix for stage cost
         ocp.cost.W_e = self.Qt  # Weight matrix for terminal cost
 
@@ -327,7 +333,7 @@ class MPCController(BaseController):
         # Set constraints
         rpm_min = 4070.3**2  # lower bound for rotor rates
         rpm_max = (4070.3 + 0.2685 * 65535) ** 2  # upper bound for rotor rates
-        thrust_lb = 0.3 * self.mass * self.g  # lower bound for thrust to avoid tumbling
+        thrust_lb = 0.3 * 0.25 * self.mass * self.g  # lower bound for thrust to avoid tumbling
         thrust_ub = self.ct * rpm_max  # upper bound for thrust as maximum achievable
         torque_xy_lb = -self.c_tau_xy * 2 * (rpm_max - rpm_min)
         torque_xy_ub = self.c_tau_xy * 2 * (rpm_max - rpm_min)
@@ -339,26 +345,28 @@ class MPCController(BaseController):
             ocp.constraints.ubu = np.array([thrust_ub, thrust_ub, thrust_ub, thrust_ub])
             ocp.constraints.idxbu = np.array([0, 1, 2, 3])
         elif self.model.name == "quadrotor_torque_mpc":
-            ocp.constraints.lbu = np.array([thrust_lb, torque_xy_lb, torque_xy_lb, torque_z_lb])
+            ocp.constraints.lbu = np.array([thrust_lb * 4, torque_xy_lb, torque_xy_lb, torque_z_lb])
             ocp.constraints.ubu = np.array([thrust_ub * 4, torque_xy_ub, torque_xy_ub, torque_z_ub])
             ocp.constraints.idxbu = np.array([1, 2, 3, 4])
         else:
             raise ValueError(f"Unknown model name {self.model.name}")
 
-        x_y_max = 2
-        z_max = 2
+        x_y_max = 3
+        z_max = 2.5
+        z_min = 0.01
+        rpy_max = 85 / 180 * np.pi
         large_val = 1e6
         ocp.constraints.lbx = np.array(
             [
                 -x_y_max,
                 -x_y_max,
-                0.01,
+                z_min,
                 -large_val,
                 -large_val,
                 -large_val,
-                -np.pi,
-                -np.pi,
-                -np.pi,
+                -rpy_max,
+                -rpy_max,
+                -rpy_max,
                 -large_val,
                 -large_val,
                 -large_val,
@@ -372,9 +380,9 @@ class MPCController(BaseController):
                 large_val,
                 large_val,
                 large_val,
-                np.pi,
-                np.pi,
-                np.pi,
+                rpy_max,
+                rpy_max,
+                rpy_max,
                 large_val,
                 large_val,
                 large_val,
@@ -410,7 +418,6 @@ class MPCController(BaseController):
 
     def updateTargetTrajectory(self) -> NDArray[np.floating]:
         """Update the target trajectory for the MPC controller."""
-        self.n_step += 1
         current_time = self.n_step * self.t_step
         t_horizon = np.linspace(
             current_time, current_time + self.n_horizon * self.t_step, self.n_horizon + 1
@@ -424,12 +431,14 @@ class MPCController(BaseController):
             last_value = self.target_trajectory(self.t_total).reshape(3, 1)
             n_repeat = np.sum(t_horizon > self.t_total)
             repeated_values = np.tile(last_value, (1, n_repeat))
-            reference_trajectory_horizon[:, -n_repeat:] = repeated_values
+            reference_trajectory_horizon[-n_repeat:, :] = repeated_values.T
         # print(reference_trajectory_horizon)
+        self.n_step += 1
         return reference_trajectory_horizon.T
 
-    def set_target_trajectory(self, t_total: float = 8) -> None:
+    def set_target_trajectory(self, t_total: float = 11) -> None:
         """Set the target trajectory for the MPC controller."""
+        self.n_step = 0  # current step for the target trajectory
         waypoints = np.array(
             [
                 [1.0, 1.0, 0.0],
@@ -447,7 +456,85 @@ class MPCController(BaseController):
         self.t_total = t_total
         t = np.linspace(0, self.t_total, len(waypoints))
         self.target_trajectory = CubicSpline(t, waypoints)
+        # Generate points along the spline for visualization
+
+        t_vis = np.linspace(0, self.t_total - 1, 100)
+        spline_points = self.target_trajectory(t_vis)
+        try:
+            # Plot the spline as a line in PyBullet
+            for i in range(len(spline_points) - 1):
+                p.addUserDebugLine(
+                    spline_points[i],
+                    spline_points[i + 1],
+                    lineColorRGB=[1, 0, 0],  # Red color
+                    lineWidth=2,
+                    lifeTime=0,  # 0 means the line persists indefinitely
+                    physicsClientId=0,
+                )
+        except p.error:
+            ...  # Ignore errors if PyBullet is not available
         return None
+
+    def setupIPOPToptimizer(self, current_state: NDArray[np.floating]):
+        """Setup the IPOPT optimizer for the MPC controller."""
+        # Define the optimization variables
+        opti = ca.Opti()
+        X = opti.variable(self.nx, self.n_horizon + 1)
+        U = opti.variable(self.nu, self.n_horizon)
+        # x_ref = opti.parameter(self.nx, self.n_horizon + 1)
+        # u_ref = opti.parameter(self.nu, 1)
+        # Slack variables for soft constraints
+        slack_x = self.soft_penalty * opti.variable(len(self.ocp.constraints.idxbx))
+        slack_u = self.soft_penalty * opti.variable(len(self.ocp.constraints.idxbu))
+
+        self.n_step = 0
+        target_trajectory = self.updateTargetTrajectory()
+        target_trajectory = np.vstack([target_trajectory, np.zeros((9, self.n_horizon + 1))])
+        u_ref = self.u_eq
+        # Define the objective function
+        obj = 0
+        for k in range(self.n_horizon):
+            x_k = X[:, k]
+            u_k = U[:, k]
+            ref_k = target_trajectory[:, k]
+            obj += ca.mtimes([(x_k - ref_k).T, self.Qs, (x_k - ref_k)]) + ca.mtimes(
+                [(u_k - u_ref).T, self.Rs, (u_k - u_ref)]
+            )
+        obj += ca.mtimes(
+            [
+                (X[:, -1] - target_trajectory[:, -1]).T,
+                self.Qt,
+                (X[:, -1] - target_trajectory[:, -1]),
+            ]
+        )
+
+        # Define the constraints
+        opti.subject_to(X[:, 0] == current_state)
+        for k in range(self.n_horizon):
+            # Dynamics constraints
+            x_k = X[:, k]
+            u_k = U[:, k]
+            x_next = X[:, k + 1]
+            f_k = self.disc_dyn(x_k, u_k)
+            opti.subject_to(x_next == f_k)
+            # State constraints
+            opti.subject_to(self.ocp.constraints.lbx <= x_k <= self.ocp.constraints.ubx)
+            # Control constraints
+            opti.subject_to(self.ocp.constraints.lbu <= u_k <= self.ocp.constraints.ubu)
+        # Final state constraints
+        opti.subject_to(self.ocp.constraints.lbx <= X[:, -1] <= self.ocp.constraints.ubx)
+
+        opti.minimize(obj)
+        # Set the IPOPT options
+        opts = {
+            "ipopt.print_level": 0,
+            "ipopt.tol": 1e-4,
+            "ipopt.max_iter": 25,
+            "ipopt.linear_solver": "mumps",
+        }
+        opti.solver("ipopt", opts)
+
+        self.opti = opti
 
     def calculateInitialGuess(self, current_state: NDArray[np.floating]):
         """Calculate the initial guess for the optimization problem.
@@ -455,74 +542,20 @@ class MPCController(BaseController):
         Args:
             current_state: The current state of the system.
         """
-        # Define the optimization variables
-        X = ca.MX.sym("X", self.nx, self.n_horizon + 1)
-        U = ca.MX.sym("U", self.nu, self.n_horizon)
-        self.n_step = 0
-        target_trajectory = self.updateTargetTrajectory()
-        target_trajectory = np.vstack([target_trajectory, np.zeros((9, self.n_horizon + 1))])
+        self.setupIPOPToptimizer(current_state=current_state)
 
-        # Define the objective function
-        obj = 0
-        for k in range(self.n_horizon):
-            x_k = X[:, k]
-            u_k = U[:, k]
-            x_next = X[:, k + 1]
-            target_k = target_trajectory[:, k]
-            obj += ca.mtimes([(x_k - target_k).T, self.Qs, (x_k - target_k)]) + ca.mtimes(
-                [u_k.T, self.Rs, u_k]
-            )
-        target_final = target_trajectory[:, self.n_horizon]
-        obj += ca.mtimes(
-            [
-                (X[:, self.n_horizon] - target_final).T,
-                self.Qt,
-                (X[:, self.n_horizon] - target_final),
-            ]
-        )
-
-        # Define the constraints
-        g = []
-        # Constraint to ensure the first state is the current state
-        g.append(X[:, 0] - current_state)
-        for k in range(self.n_horizon):
-            x_k = X[:, k]
-            u_k = U[:, k]
-            x_next = X[:, k + 1]
-            f_k = self.disc_dyn(x_k, u_k)
-            g.append(x_next - f_k)
-
-        # Flatten the constraints
-        g = ca.vertcat(*g)
-
-        # Define the NLP problem
-        nlp = {"x": ca.vertcat(ca.reshape(X, -1, 1), ca.reshape(U, -1, 1)), "f": obj, "g": g}
-
-        # Set IPOPT options
-        opts = {
-            "ipopt.print_level": 0,
-            "ipopt.tol": 1e-4,
-            "ipopt.max_iter": 25,
-            "ipopt.linear_solver": "mumps",
-        }
-
-        # Create the IPOPT solver
-        solver = ca.nlpsol("solver", "ipopt", nlp, opts)
-
-        # Define the initial guess
-        x0 = np.zeros((self.nx, self.n_horizon + 1))
-        u0 = np.zeros((self.nu, self.n_horizon))
-        initial_guess = np.concatenate([x0.flatten(), u0.flatten()])
-
-        # Solve the optimization problem
-        sol = solver(x0=initial_guess, lbg=0, ubg=0)
+        sol = self.opti.solve()
 
         # Extract the solution
         x_sol = np.reshape(
-            sol["x"][: self.nx * (self.n_horizon + 1)], (self.nx, self.n_horizon + 1)
+            sol["X"][: self.nx * (self.n_horizon + 1)], (self.nx, self.n_horizon + 1)
         )
-        u_sol = np.reshape(sol["x"][self.nx * (self.n_horizon + 1) :], (self.nu, self.n_horizon))
+        u_sol = np.reshape(sol["U"][self.nx * (self.n_horizon + 1) :], (self.nu, self.n_horizon))
 
         # Set the initial guess for the acados optimizer
         self.x_guess = x_sol
         self.u_guess = u_sol
+
+        # Debugging information
+        print("Initial guess for states (X):", x_sol)
+        print("Initial guess for controls (U):", u_sol)
